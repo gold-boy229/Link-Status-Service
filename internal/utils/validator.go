fc5604e1a6e01eba1743e8ba46c14d6c20c046e3
gold-boy229 <go1d_boy@bk.ru>
2025-12-10 00:46:35 +0300

    Включил линтер funcorder Исправил все предупреждения, связанные с ним

--- internal/utils/validator.go
+++ internal/utils/validator.go
@@ -11,14 +11,14 @@ type customValidator struct {
 	validator *validator.Validate
 }
 
+func NewCustomValidator() *customValidator {
+	return &customValidator{validator: validator.New(validator.WithRequiredStructEnabled())}
+}
+
 func (cv *customValidator) Validate(i interface{}) error {
 	if err := cv.validator.Struct(i); err != nil {
 		// Return the error so Echo's error handler can process it
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 	return nil
-}
-
-func NewCustomValidator() *customValidator {
-	return &customValidator{validator: validator.New(validator.WithRequiredStructEnabled())}
 }
